443b349124dadf2c39e8d063248c60d302d203b9
Felipe Oliveira <felipeeflorentino@gmail.com>
2024-10-31 04:25:16 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,21 +1,40 @@
 from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import format_html
+from blog.models import Post
 
 
 user_model = get_user_model()
 register = template.Library()
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
 
 
-@register.filter
-def author_details(author, current_user=None):
-    if not isinstance(author, user_model):
-        # return empty string as safe default
-        return ""
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
 
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
 
     if author == current_user:
-      return format_html("<strong>me</strong>")
+        return format_html("<strong>me</strong>")
 
     if author.first_name and author.last_name:
         name = f"{author.first_name} {author.last_name}"
@@ -29,7 +48,12 @@ def author_details(author, current_user=None):
         prefix = ""
         suffix = ""
 
-    return format_html('{}{}{}', prefix, name, suffix)
+    return format_html("{}{}{}", prefix, name, suffix)
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
 
 
 
