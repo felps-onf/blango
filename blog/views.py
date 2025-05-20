c60d9914b44a1540ab47252985530961cff2d37c
Felipe Oliveira <felipeeflorentino@gmail.com>
2025-05-20 20:04:03 +0000

    Finish Javascript intro and fundamentals

--- blog/views.py
+++ blog/views.py
@@ -42,3 +42,7 @@ def post_detail(request, slug):
         request, "blog/post-detail.html", {"post": post, "comment_form": comment_form}
     )
 
+
+def post_table(request):
+    return render(request, "blog/post-table.html")
+
